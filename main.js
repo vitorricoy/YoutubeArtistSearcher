83a9d6db4e173b1b9824c72c70caf88027ab2ce7
Unknown <vitorricoy@yahoo.com.br>
2017-12-10 11:51:45 -0200

    Fix url

--- main.js
+++ main.js
@@ -1,57 +1,9 @@
-var OAUTH2_CLIENT_ID = 'AIzaSyDO2qxaH_63EBQbSR9cQhRWQzRcBSNpSj8';
-var OAUTH2_SCOPES = [
-  'https://www.googleapis.com/auth/youtube'
-];
-
-googleApiClientReady = function() {
-  gapi.auth.init(function() {
-    window.setTimeout(checkAuth, 1);
-  });
-}
-
-function checkAuth() {
-  gapi.auth.authorize({
-    client_id: OAUTH2_CLIENT_ID,
-    scope: OAUTH2_SCOPES,
-    immediate: true
-  }, handleAuthResult);
-}
-
-function handleAuthResult(authResult) {
-  if (authResult && !authResult.error) {
-    $('.pre-auth').hide();
-    $('.post-auth').show();
-    loadAPIClientInterfaces();
-  } else {
-    $('#login-link').click(function() {
-      gapi.auth.authorize({
-        client_id: OAUTH2_CLIENT_ID,
-        scope: OAUTH2_SCOPES,
-        immediate: false
-        }, handleAuthResult);
-    });
-  }
-}
-
-function loadAPIClientInterfaces() {
-  gapi.client.load('youtube', 'v3', function() {
-    handleAPILoaded();
-  });
-}
-
-function handleAPILoaded() {
-  $('#search-button').attr('disabled', false);
-}
 
 function search() {
-  var q = $('#query').val();
-  var request = gapi.client.youtube.search.list({
-    q: q,
-    part: 'snippet'
-  });
-
-  request.execute(function(response) {
-    var str = JSON.stringify(response.result);
+  let q = $('#query').val();
+  let url='https://www.googleapis.com/youtube/v3/search?part=snippet&videoCategoryId=10&type=video&key=AIzaSyCtohEkJ6mCItORJn4nSlC3y2LEuHMxyOs'
+  $.getJSON(url + "&q=" + q, function (json) {
+  	var str = JSON.stringify(response.result);
     $('#search-container').html('<pre>' + str + '</pre>');
-  });
+  }
 }
