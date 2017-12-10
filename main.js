5cded33f96d9917c1f2544a439fc2d08f0122a26
Unknown <vitorricoy@yahoo.com.br>
2017-12-10 11:54:58 -0200

    Lack of attention

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function search() {
   let q = $('#query').val();
   let url='https://www.googleapis.com/youtube/v3/search?part=snippet&videoCategoryId=10&type=video&key=AIzaSyCtohEkJ6mCItORJn4nSlC3y2LEuHMxyOs';
   $.getJSON(url + "&q=" + q, function (json) {
-  	var str = JSON.stringify(response.result);
+  	var str = JSON.stringify(json);
     $('#search-container').html('<pre>' + str + '</pre>');
   });
 }
