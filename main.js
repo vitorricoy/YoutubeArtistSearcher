05c49894a1a8b37eba4f0f6a547bc5fa27674916
Unknown <vitorricoy@yahoo.com.br>
2017-12-10 14:33:43 -0200

    Optimization

--- main.js
+++ main.js
@@ -6,69 +6,44 @@ function search() {
   let q = $('#query').val();
   let url='https://www.googleapis.com/youtube/v3/search?part=id&maxResults=50&videoCategoryId=10&type=video&key=AIzaSyCtohEkJ6mCItORJn4nSlC3y2LEuHMxyOs';
   let quantity = $('#quantity').val();
-  $.getJSON(url + "&q=" + q, function (json) {
-  	json.items.forEach(function(item){
-  		ids.push(item.id.videoId);
-  	});
-  	console.log(ids);
-  	let stringIds = "";
-  	for(let i=0; i<ids.length; i++){
-  		stringIds+=ids[i];
-  		if(i!=ids.length-1){
-  			stringIds+=',';
-  		}
-  	}
-  	let urlDuration = 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&key=AIzaSyCtohEkJ6mCItORJn4nSlC3y2LEuHMxyOs'
-    $.getJSON(urlDuration + "&id=" + stringIds, function (jsonDuration) {
-    	ids=[];
-    	jsonDuration.items.forEach(function(item){
-    		let duracao = item.contentDetails.duration;
-    		duracao = convertISO8601ToSeconds(duracao);
-    		if(duracao>=60 && duracao<=600){
-    			ids.push(item.id);
-    		}
-    	});
-    	console.log(ids);
-    	let tokenAtual=json.nextPageToken;
-    	let novosIds;
-    	while(quantity>ids.length){
-    		console.log(ids);
-    		novosIds=[];
-    		$.getJSON(url + "&q=" + q + "&pageToken=" + tokenAtual, function (jsonLoop) {
-			  	jsonLoop.items.forEach(function(item){
-			  		novosIds.push(item.id.videoId);
-			  	});
-			  	console.log(novosIds);
-			  	let stringIds = "";
-			  	for(let i=0; i<ids.length; i++){
-			  		stringIds+=ids[i];
-			  		if(i!=ids.length-1){
-			  			stringIds+=',';
-			  		}
-			  	}
-			  	let urlDuration = 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&key=AIzaSyCtohEkJ6mCItORJn4nSlC3y2LEuHMxyOs'
-			    $.getJSON(urlDuration + "&id=" + stringIds, function (jsonDuration) {
-			    	novosIds=[];
-			    	jsonDuration.items.forEach(function(item){
-			    		let duracao = item.contentDetails.duration;
-			    		duracao = convertISO8601ToSeconds(duracao);
-			    		if(duracao>=60 && duracao<=600){
-			    			novosIds.push(item.id);
-			    		}
-			    	});
-			    	console.log(novosIds);
-			    	ids.concat(novosIds);
-			    	tokenAtual=jsonLoop.nextPageToken;
-			    });
+  let tokenAtual=null;
+  let novosIds;
+  while(quantity>ids.length){
+    novosIds=[];
+    $.getJSON(url + "&q=" + q + ((tokenAtual==null)?(''):("&pageToken=" + tokenAtual)), function (jsonLoop) {
+		console.log(jsonLoop);
+		jsonLoop.items.forEach(function(item){
+			novosIds.push(item.id.videoId);
+		});
+		console.log(novosIds);
+		let stringIds = "";
+		for(let i=0; i<ids.length; i++){
+			stringIds+=ids[i];
+			if(i!=ids.length-1){
+			  	stringIds+=',';
+			}
+		}
+		let urlDuration = 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&key=AIzaSyCtohEkJ6mCItORJn4nSlC3y2LEuHMxyOs'
+		$.getJSON(urlDuration + "&id=" + stringIds, function (jsonDuration) {
+			novosIds=[];
+			jsonDuration.items.forEach(function(item){
+			    let duracao = item.contentDetails.duration;
+			    duracao = convertISO8601ToSeconds(duracao);
+			    if(duracao>=60 && duracao<=600){
+			    	novosIds.push(item.id);
+			    }
 			});
-			sleep(1000);
-			console.log('fim sleep');
-    	}
-    	while(ids.length>quantity){
-    		ids.pop();
-    	}
-    });
-  });
+			console.log(novosIds);
+			ids.concat(novosIds);
+			tokenAtual=jsonLoop.nextPageToken;
+		});
+	});
+	sleep(500);
+	console.log('fim sleep');
+  }
+  while(ids.length>quantity){
+    ids.pop();
+  }
   console.log(ids);
 }
 
